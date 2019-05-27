b8b8e90f3f263984b2b14ca7edade1e899830b23
Nareilson <narotkd@gmail.com>
2019-05-26 23:44:18 -0300

    Retirando a logica do OnCreadView para evitar NullPoint

--- app/src/main/java/com/example/mykotlinaplication/Activity/TesteFragment.kt
+++ app/src/main/java/com/example/mykotlinaplication/Activity/TesteFragment.kt
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 import com.example.mykotlinaplication.R
 import io.reactivex.disposables.CompositeDisposable
@@ -14,8 +13,7 @@ import kotlinx.android.synthetic.main.fragment_teste.*
 
 
 class TesteFragment : Fragment() {
-    val button:Button? = null
-    val text:TextView?=null
+    val text: TextView? = null
     val obs = PublishSubject.create<String>()
     val dispose = CompositeDisposable()
     var cont = 0
@@ -24,20 +22,22 @@ class TesteFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_teste, container, false)
-        button?.findViewById<Button>(R.id.buttonTeste)
-        text?.findViewById<TextView>(R.id.textViewTeste)
+        return inflater.inflate(R.layout.fragment_teste, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        text?.findViewById<TextView>(R.id.textViewTeste)
         dispose.add(
-            obs.subscribe({t ->textViewTeste.text = t },{e ->textViewTeste.text = "erro"})
+            obs.subscribe({ t -> textViewTeste.text = t }, { e -> textViewTeste.text = "erro" })
         )
-        button?.setOnClickListener {
+
+        buttonTeste.setOnClickListener {
             obs.onNext("Mensagem ${cont++}")
         if(cont == 10){
             obs.onComplete()
-        }}
-        // Inflate the layout for this fragment
-        return view
+        }
+        }
     }
 
     override fun onDestroy() {
